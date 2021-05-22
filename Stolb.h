d8aad8e4fd6b59a8c0d061d6db6f2335d9175bf1
ralPeople <84584415+ralPeople@users.noreply.github.com>
2021-05-22 18:35:53 +0400

    Update Stolb.h

--- Stolb.h
+++ Stolb.h
@@ -1,21 +1,40 @@
 #include "Graphics.h"
+#include <vector>
 
 class Stolb
 {
 private:
-	int x0, y0;
+	int x;
 	int h, w;
 	int val;
+	int n, a; // количество столбов и коэфициент перевода
 	COLORREF color = RGB(255, 255, 255);
-
 public:
-	Stolb();
+	void set(int,int,int,int,int,int);
 	void VisSt();
-
 };
 
 void Stolb::VisSt()
 {
 	Graphics::Set_pen(RGB(0, 191, 255), 3);
 	Graphics::Set_brush(RGB(255, 255, 255), 1);
+	Rectangle(Graphics::dc, x, h - 0, x + w / n, h - a * val);
 }
+
+void Stolb::set(int _x,int _h,int _w,int _n,int _a,int _val)
+{
+	x = _x;
+	h = _h;
+	w = _w;
+	val = _val;
+	n = _n;
+	a = _a;
+	val = _val;
+}
+
+void draw(vector<Stolb>v)
+{
+	for (int i = 0; i < v.size(); i++)
+		v[i].VisSt();
+}
+
